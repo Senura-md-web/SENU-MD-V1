60c946877bc2679ebcf0a532a6f7d8113f978110
Senura-md-web <Senuradineth91@gmail.com>
2025-06-30 17:09:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// Created by SENU MD
+// wa.me/94XXXXXXXXX
+
+const { spawn } = require('child_process');
+const path = require('path');
+
+function start() {
+    console.log('🔁 Starting SENU MD Bot...');
+
+    let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)];
+    console.log('Starting with args:\n' + [process.argv[0], ...args].join('\n'));
+
+    let p = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    })
+    .on('message', data => {
+        if (data === 'reset') {
+            console.log('♻️ Restarting SENU MD Bot...');
+            p.kill();
+            start();
+        }
+    })
+    .on('exit', code => {
+        console.error('❌ Bot exited with code:', code);
+        if (code === '.' || code === 1 || code === 0) {
+            console.log('🔄 Auto-restarting SENU MD Bot...');
+            start();
+        }
+    });
+}
+
+start();
